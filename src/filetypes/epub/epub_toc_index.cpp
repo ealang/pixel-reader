4996a3c80fddad4dba5037d139e25c68a4b079b7
eric <ealang@gmail.com>
2023-09-07 22:46:54 -0400

    Cleanup toc index

--- src/filetypes/epub/epub_toc_index.cpp
+++ src/filetypes/epub/epub_toc_index.cpp
@@ -1,9 +1,46 @@
 #include "./epub_toc_index.h"
+
+#include "./epub_doc_index.h"
 #include "doc_api/token_addressing.h"
 
 #include <iostream>
 
 #define DEBUG 0
+
+struct TocItemProgressLookup
+{
+    uint32_t total_size = 0;                             // Total size of the toc item in units of address space
+    std::vector<DocAddr> spine_to_start_addr;            // Start address for the toc item within each spine entry
+    std::vector<uint32_t> spine_to_cumulative_size;      // Total size of the toc item at the start of each spine entry
+};
+
+struct TocItemCache
+{
+    std::string display_name;
+    uint32_t indent_level;
+    uint32_t spine_start_index;
+    std::string token_id_link;
+
+    // caching
+    mutable bool start_address_is_valid = false;
+    mutable DocAddr start_address = 0;  // address of first token for this toc item
+
+    mutable bool progress_is_valid = false;
+    mutable TocItemProgressLookup progress {0, {}, {}};
+};
+
+struct EpubTocIndexState
+{
+    EpubDocIndex &doc_index;
+    std::vector<TocItemCache> toc;
+
+    mutable uint32_t cached_toc_index = 0;
+    mutable DocAddr cached_toc_index_start_address = -1;
+    mutable DocAddr cached_toc_index_upper_address = -1;
+
+    EpubTocIndexState(EpubDocIndex &doc_index) :
+        doc_index(doc_index) {}
+};
 
 namespace {
 
@@ -90,6 +127,39 @@ void flatten_navmap_to_toc(
     );
 }
 
+// Fallback option
+void fallback_convert_spine_to_toc(
+    const PackageContents &package,
+    std::vector<TocItemCache> &out_toc
+)
+{
+    for (uint32_t spine_idx = 0; spine_idx < package.spine_ids.size(); ++spine_idx)
+    {
+        const auto &doc_id = package.spine_ids[spine_idx];
+        auto item_it = package.id_to_manifest_item.find(doc_id);
+        if (item_it != package.id_to_manifest_item.end())
+        {
+            if (item_it->second.media_type == APPLICATION_XHTML_XML)
+            {
+                out_toc.emplace_back(TocItemCache {
+                    std::filesystem::path(item_it->second.href).filename(),  // fallback name
+                    0,
+                    spine_idx,
+                    ""
+                });
+            }
+            else
+            {
+                std::cerr << "Skipping toc item of media type " << item_it->second.media_type << std::endl;
+            }
+        }
+        else
+        {
+            std::cerr << "Failed to find spine doc " << doc_id << " in manifest" << std::endl;
+        }
+    }
+}
+
 // Address above all documents.
 DocAddr spine_upper_address(const EpubDocIndex &doc_index)
 {
@@ -108,9 +178,7 @@ DocAddr document_upper_address(const EpubDocIndex &doc_index, uint32_t spine_ind
     return make_address(spine_index);
 }
 
-}  // namespace
-
-DocAddr EpubTocIndex::resolve_start_address(uint32_t item_index) const
+DocAddr resolve_start_address(uint32_t item_index, const EpubDocIndex &doc_index, const std::vector<TocItemCache> &toc)
 {
     if (item_index >= toc.size())
     {
@@ -128,17 +196,13 @@ DocAddr EpubTocIndex::resolve_start_address(uint32_t item_index) const
     if (!toc_item.token_id_link.empty())
     {
         // Need to match fragment
-        bool found = false;
-        for (const auto &id: doc_index.elem_id_to_address(toc_item.spine_start_index))
+        const auto &elem_id_to_addr = doc_index.elem_id_to_address(toc_item.spine_start_index);
+        auto it = elem_id_to_addr.find(toc_item.token_id_link);
+        if (it != elem_id_to_addr.end())
         {
-            if (id.first == toc_item.token_id_link)
-            {
-                toc_item.start_address = id.second;
-                found = true;
-                break;
-            }
+            toc_item.start_address = it->second;
         }
-        if (!found)
+        else
         {
             #if DEBUG
             std::cerr << "Failed to find id=" << toc_item.token_id_link << " in spine " << toc_item.spine_start_index << std::endl;
@@ -150,7 +214,7 @@ DocAddr EpubTocIndex::resolve_start_address(uint32_t item_index) const
     return toc_item.start_address;
 }
 
-DocAddr EpubTocIndex::resolve_upper_address(uint32_t item_index) const
+DocAddr resolve_upper_address(uint32_t item_index, const EpubDocIndex &doc_index, const std::vector<TocItemCache> &toc)
 {
     static const DocAddr null_address = make_address();
 
@@ -162,12 +226,12 @@ DocAddr EpubTocIndex::resolve_upper_address(uint32_t item_index) const
 
     if (item_index + 1 < toc.size())
     {
-        return resolve_start_address(item_index + 1);
+        return resolve_start_address(item_index + 1, doc_index, toc);
     }
     return spine_upper_address(doc_index);
 }
 
-const TocItemProgressLookup &EpubTocIndex::resolve_progress_lookup(uint32_t item_index) const
+const TocItemProgressLookup &resolve_progress_lookup(uint32_t item_index, const EpubDocIndex &doc_index, const std::vector<TocItemCache> &toc)
 {
     if (item_index >= toc.size())
     {
@@ -186,8 +250,8 @@ const TocItemProgressLookup &EpubTocIndex::resolve_progress_lookup(uint32_t item
         progress.spine_to_start_addr.resize(doc_index.spine_size());
         progress.spine_to_cumulative_size.resize(doc_index.spine_size());
 
-        DocAddr start_address = resolve_start_address(item_index);
-        DocAddr upper_address = resolve_upper_address(item_index);
+        DocAddr start_address = resolve_start_address(item_index, doc_index, toc);
+        DocAddr upper_address = resolve_upper_address(item_index, doc_index, toc);
 
         uint32_t start_spine = get_chapter_number(start_address);
         uint32_t end_spine = get_chapter_number(upper_address);
@@ -220,39 +284,19 @@ const TocItemProgressLookup &EpubTocIndex::resolve_progress_lookup(uint32_t item
     return progress;
 }
 
+} // namespace
+
 EpubTocIndex::EpubTocIndex(const PackageContents &package, const std::vector<NavPoint> &navmap, EpubDocIndex &doc_index)
-    : doc_index(doc_index)
+    : state(std::make_unique<EpubTocIndexState>(doc_index))
 {
+    auto &toc = state->toc;
+
     flatten_navmap_to_toc(package, navmap, toc);
 
     if (toc.empty())
     {
         std::cerr << "Falling back to spine for TOC" << std::endl;
-        for (uint32_t spine_idx = 0; spine_idx < package.spine_ids.size(); ++spine_idx)
-        {
-            const auto &doc_id = package.spine_ids[spine_idx];
-            auto item_it = package.id_to_manifest_item.find(doc_id);
-            if (item_it != package.id_to_manifest_item.end())
-            {
-                if (item_it->second.media_type == APPLICATION_XHTML_XML)
-                {
-                    toc.emplace_back(TocItemCache {
-                        std::filesystem::path(item_it->second.href).filename(),  // fallback name
-                        0,
-                        spine_idx,
-                        ""
-                    });
-                }
-                else
-                {
-                    std::cerr << "Skipping toc item of media type " << item_it->second.media_type << std::endl;
-                }
-            }
-            else
-            {
-                std::cerr << "Failed to find spine doc " << doc_id << " in manifest" << std::endl;
-            }
-        }
+        fallback_convert_spine_to_toc(package, toc);
     }
 
     #if DEBUG
@@ -282,13 +326,18 @@ EpubTocIndex::EpubTocIndex(const PackageContents &package, const std::vector<Nav
     }
 }
 
+EpubTocIndex::~EpubTocIndex()
+{
+}
+
 uint32_t EpubTocIndex::toc_size() const
 {
-    return toc.size();
+    return state->toc.size();
 }
 
 std::string EpubTocIndex::toc_item_display_name(uint32_t toc_item_index) const
 {
+    const auto &toc = state->toc;
     if (toc_item_index >= toc.size())
     {
         return "";
@@ -298,6 +347,7 @@ std::string EpubTocIndex::toc_item_display_name(uint32_t toc_item_index) const
 
 uint32_t EpubTocIndex::toc_item_indent_level(uint32_t toc_item_index) const
 {
+    const auto &toc = state->toc;
     if (toc_item_index >= toc.size())
     {
         return 0;
@@ -307,11 +357,16 @@ uint32_t EpubTocIndex::toc_item_indent_level(uint32_t toc_item_index) const
 
 DocAddr EpubTocIndex::get_toc_item_address(uint32_t toc_item_index) const
 {
-    return resolve_start_address(toc_item_index);
+    return resolve_start_address(toc_item_index, state->doc_index, state->toc);
 }
 
 std::optional<uint32_t> EpubTocIndex::get_toc_item_index(const DocAddr &address) const
 {
+    const auto &toc = state->toc;
+    auto &cached_toc_index = state->cached_toc_index;
+    auto &cached_toc_index_start_address = state->cached_toc_index_start_address;
+    auto &cached_toc_index_upper_address = state->cached_toc_index_upper_address;
+
     if (toc.empty())
     {
         return std::nullopt;
@@ -333,12 +388,12 @@ std::optional<uint32_t> EpubTocIndex::get_toc_item_index(const DocAddr &address)
         if (spine_index >= toc_item_spine_start && spine_index <= toc_item_spine_end)
         {
             // Next chapter may not start at the beginning of the doc. Need to check addresses.
-            const auto &toc_item_start_addr = resolve_start_address(i);
-            const auto &toc_item_upper_addr = resolve_upper_address(i);
+            const auto &toc_item_start_addr = resolve_start_address(i, state->doc_index, state->toc);
+            const auto &toc_item_upper_addr = resolve_upper_address(i, state->doc_index, state->toc);
             if (address >= toc_item_start_addr && address < toc_item_upper_addr)
             {
                 cached_toc_index = i;
-                cached_toc_index_start_address  = toc_item_start_addr;
+                cached_toc_index_start_address = toc_item_start_addr;
                 cached_toc_index_upper_address = toc_item_upper_addr;
                 return i;
             }
@@ -347,7 +402,7 @@ std::optional<uint32_t> EpubTocIndex::get_toc_item_index(const DocAddr &address)
 
     {
         uint32_t i = toc.size() - 1;
-        if (address >= resolve_start_address(i))
+        if (address >= resolve_start_address(i, state->doc_index, state->toc))
         {
             return i;
         }
@@ -360,12 +415,12 @@ std::optional<uint32_t> EpubTocIndex::get_toc_item_index(const DocAddr &address)
 std::pair<uint32_t, uint32_t> EpubTocIndex::get_toc_item_progress(const DocAddr &address) const
 {
     auto toc_index = get_toc_item_index(address);
-    if (!toc_index || *toc_index >= toc.size())
+    if (!toc_index || *toc_index >= state->toc.size())
     {
         return {0, 0};
     }
 
-    const auto &progress = resolve_progress_lookup(*toc_index);
+    const auto &progress = resolve_progress_lookup(*toc_index, state->doc_index, state->toc);
     uint32_t spine_index = get_chapter_number(address);
 
     uint32_t pos = (
--- src/filetypes/epub/epub_toc_index.h
+++ src/filetypes/epub/epub_toc_index.h
@@ -1,54 +1,29 @@
 #ifndef EPUB_TOC_INDEX_H_
 #define EPUB_TOC_INDEX_H_
 
+#include "./epub_metadata.h"
+#include "doc_api/doc_addr.h"
+
 #include <cstdint>
+#include <memory>
 #include <optional>
 #include <vector>
 
-#include "./epub_doc_index.h"
-#include "doc_api/doc_addr.h"
-
-struct TocItemProgressLookup
-{
-    uint32_t total_size = 0;                             // Total size of the toc item in units of address space
-    std::vector<DocAddr> spine_to_start_addr;            // Start address for the toc item within each spine entry
-    std::vector<uint32_t> spine_to_cumulative_size;      // Total size of the toc item at the start of each spine entry
-};
-
-struct TocItemCache
-{
-    std::string display_name;
-    uint32_t indent_level;
-    uint32_t spine_start_index;
-    std::string token_id_link;
-
-    // caching
-    mutable bool start_address_is_valid = false;
-    mutable DocAddr start_address = 0;  // address of first token for this toc item
-
-    mutable bool progress_is_valid = false;
-    mutable TocItemProgressLookup progress {0, {}, {}};
-};
+struct EpubTocIndexState;
+struct PackageContents;
+struct EpubDocIndex;
 
 // Provide information about table of contents entries.
 // Lazy load from zip as needed.
 class EpubTocIndex
 {
-    EpubDocIndex &doc_index;
-    std::vector<TocItemCache> toc;
-
-    DocAddr resolve_start_address(uint32_t item_index) const;
-    DocAddr resolve_upper_address(uint32_t item_index) const;
-    const TocItemProgressLookup &resolve_progress_lookup(uint32_t item_index) const;
-
-    mutable uint32_t cached_toc_index = 0;
-    mutable DocAddr cached_toc_index_start_address = -1;
-    mutable DocAddr cached_toc_index_upper_address = -1;
+    std::unique_ptr<EpubTocIndexState> state;
 
 public:
     EpubTocIndex(const PackageContents &package, const std::vector<NavPoint> &navmap, EpubDocIndex &doc_index);
     EpubTocIndex(const EpubTocIndex &) = delete;
     EpubTocIndex &operator=(const EpubTocIndex &) = delete;
+    virtual ~EpubTocIndex();
 
     uint32_t toc_size() const;
     std::string toc_item_display_name(uint32_t toc_item_index) const;
