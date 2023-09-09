341869bdff2f60a6d8a851aba26302945c25399d
eric <ealang@gmail.com>
2023-09-09 02:44:41 -0400

    Add caching to bulk load test

--- src/sandbox/bulk_load_test.cpp
+++ src/sandbox/bulk_load_test.cpp
@@ -1,5 +1,8 @@
 #include "doc_api/token_addressing.h"
 #include "filetypes/open_doc.h"
+#include "reader/config.h"
+#include "reader/ss_doc_reader_cache.h"
+#include "reader/state_store.h"
 #include "util/timer.h"
 
 #include <filesystem>
@@ -15,12 +18,12 @@ struct Stats
     std::list<std::pair<std::string, uint32_t>> load_times;
 };
 
-void load_file(std::filesystem::path path, Stats &stats)
+void load_file(std::filesystem::path path, Stats &stats, DocReaderCache &cache)
 {
     Timer t;
 
     auto reader = create_doc_reader(path);
-    if (!reader || !reader->open())
+    if (!reader || !reader->open(cache))
     {
         std::cerr << "Unable to open " << path.filename() << std::endl;
         return;
@@ -37,11 +40,14 @@ void bulk_load_test(std::string dir_path)
 {
     if (std::filesystem::exists(dir_path) && std::filesystem::is_directory(dir_path))
     {
+        StateStore store("store");
+        SSDocReaderCache cache(store);
+
         Stats stats;
 
         for (const auto& entry: std::filesystem::directory_iterator(dir_path))
         {
-            load_file(entry.path(), stats);
+            load_file(entry.path(), stats, cache);
         }
 
         uint32_t total_load_time = 0;
@@ -55,6 +61,12 @@ void bulk_load_test(std::string dir_path)
         std::cerr << std::endl;
         std::cerr << "Total files: " << stats.load_times.size() << std::endl;
         std::cerr << "Total time: " << total_load_time << std::endl;
+
+        {
+            Timer t;
+            store.flush();
+            std::cerr << "Flush time: " << t.elapsed_ms() << std::endl;
+        }
     }
     else
     {
